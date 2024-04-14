f7e68afd62fbe768850a0bbc6950efe5c8873987
CodeProSpecialist <droark28@hotmail.com>
2024-04-14 10:57:48 -0500

    updates

--- install.sh
+++ install.sh
@@ -9,5 +9,8 @@ sudo apt install -y python3
 # Install pip3
 sudo apt install -y python3-pip libhdf5-dev
 
+# making sure python3.11 can install packages by renaming EXTERNALLY-MANAGED to EXTERNALLY-MANAGED.old
+sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
+
 # Install Python packages
-pip3 install yfinance numpy scikit-learn tensorflow alpaca-trade-api ta
+pip3 install yfinance numpy scikit-learn tensorflow alpaca-trade-api ta TensorRT
