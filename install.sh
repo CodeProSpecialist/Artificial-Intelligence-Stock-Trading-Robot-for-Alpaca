c0f53720969ba13e0d574cafda0e152d5b355ec2
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2024-04-14 02:19:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt install -y python3
 
 # Install pip3
-sudo apt install -y python3-pip python3-h5py
+sudo apt install -y python3-pip libhdf5-dev
 
 # Install Python packages
 pip3 install yfinance numpy scikit-learn tensorflow alpaca-trade-api ta
