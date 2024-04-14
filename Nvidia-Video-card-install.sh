5187e926043b30cf7be70d1fd4f47d1b0a461554
CodeProSpecialist <droark28@hotmail.com>
2024-04-14 12:04:16 -0500

    updates

--- Nvidia-Video-card-install.sh
+++ Nvidia-Video-card-install.sh
@@ -3,6 +3,15 @@
 # Update package list
 sudo apt update
 
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu$(lsb_release -cs) main"
+
+sudo apt update
+
+sudo apt-get install libnvinfer7
+
+sudo apt-get install tensorrt
+
+
 # Install Python 3
 sudo apt install -y python3
 
@@ -12,8 +21,8 @@ sudo apt install -y python3-pip libhdf5-dev
 # making sure python3.11 can install packages by renaming EXTERNALLY-MANAGED to EXTERNALLY-MANAGED.old
 sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
 
+
+
 # Install Python packages
 pip3 install yfinance numpy scikit-learn tensorflow alpaca-trade-api ta TensorRT
 
-# ignore errors about not finding TensorRT to run this code without NVidia video cards on
-# every Linux device like a Raspberry Pi.
