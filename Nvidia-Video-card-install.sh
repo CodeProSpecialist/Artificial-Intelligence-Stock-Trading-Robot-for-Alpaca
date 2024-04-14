8608ce2fc8255c331edf25ad47df35afeab5cd27
CodeProSpecialist <droark28@hotmail.com>
2024-04-14 13:52:12 -0500

    updates

--- Nvidia-Video-card-install.sh
+++ Nvidia-Video-card-install.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
-# the following is for Ubuntu 22.04 LTS
-
 # Update package list
 sudo apt update
 
+# Manually add the NVIDIA CUDA repository key
 wget -O /tmp/7fa2af80.pub https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/7fa2af80.pub
 sudo apt-key add /tmp/7fa2af80.pub
-
-# Add NVIDIA CUDA repository key
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/Release.gpg
-sudo apt-key add Release.gpg
-
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/7fa2af80.pub
-
 
 # Add NVIDIA CUDA repository keyring
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
-
 
 # Add NVIDIA CUDA repository
 sudo sh -c 'echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
@@ -27,33 +18,20 @@ sudo sh -c 'echo "deb https://developer.download.nvidia.com/compute/cuda/repos/u
 sudo apt-get update
 
 # Clean up
-rm Release.gpg cuda-keyring_1.1-1_all.deb
+rm /tmp/7fa2af80.pub cuda-keyring_1.1-1_all.deb
 
-# Update package lists
-sudo apt-get update
-
-# Clean up
-rm cuda-keyring_1.0-1_all.deb
-
-sudo apt update
-
+# Continue with other installations
+# Install NVIDIA CUDA packages
 sudo apt-get install nvidia-cudnn nvidia-cuda-toolkit-gcc cuda-toolkit libcudnn
 
-sudo apt-get install tensorrt
+# Install TensorRT
+sudo apt-get install tensorrt libnvinfer7
 
-sudo apt-get install libnvinfer7
+# Install Python 3 and pip3
+sudo apt install -y python3 python3-pip libhdf5-dev
 
-# Install Python 3
-sudo apt install -y python3
-
-# Install pip3
-sudo apt install -y python3-pip libhdf5-dev
-
-# making sure python3.11 can install packages by renaming EXTERNALLY-MANAGED to EXTERNALLY-MANAGED.old
+# Rename EXTERNALLY-MANAGED to EXTERNALLY-MANAGED.old to allow Python 3.11 to install packages
 sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
-
-
 
 # Install Python packages
 pip3 install yfinance numpy scikit-learn tensorflow alpaca-trade-api ta TensorRT
-
