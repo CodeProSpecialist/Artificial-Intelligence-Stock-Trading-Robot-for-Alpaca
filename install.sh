d92829feac962979c976670c6af7c6132e672c89
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2024-04-21 00:49:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,16 +9,26 @@ sudo apt install -y python3 python3-pip
 # Install required packages
 sudo apt install -y libhdf5-dev
 
+# Create and activate virtual environment in the user's home directory
+HOME_VENV_PATH="$HOME/My-Python-Virtual-Environment-Packages"
+VENV_PATH="$HOME_VENV_PATH/venv"
 
-# Install Python packages
-pip3 install yfinance numpy scikit-learn alpaca-trade-api pytz ta-lib
+# Check if the virtual environment directory exists, if not, create it
+if [ ! -d "$VENV_PATH" ]; then
+    mkdir -p "$VENV_PATH"
+fi
 
-# Install PyTorch
-pip3 install torch torchvision
+# Create the virtual environment
+python3 -m venv "$VENV_PATH"
+
+# Activate the virtual environment
+source "$VENV_PATH/bin/activate"
+
+# Install Python packages within the virtual environment
+pip install yfinance numpy scikit-learn alpaca-trade-api pytz ta-lib torch torchvision
 
 # Install TA-Lib dependencies
 echo "Installing TA-Lib dependencies ..."
-
 sudo apt-get install libatlas-base-dev gfortran -y
 
 # Download and install TA-Lib
@@ -36,9 +46,11 @@ cd ..
 rm -r ta-lib
 rm ta-lib-0.4.0-src.tar.gz
 
-
-# Add '/home/x800/.local/bin' to PATH if not already present
-if ! grep -q '/home/x800/.local/bin' ~/.bashrc; then
-    echo 'export PATH="$PATH:/home/x800/.local/bin"' >> ~/.bashrc
+# Add the virtual environment's bin directory to PATH if not already present
+if ! grep -q "$HOME_VENV_PATH/venv/bin" ~/.bashrc; then
+    echo 'export PATH="$PATH:'"$HOME_VENV_PATH"'/venv/bin"' >> ~/.bashrc
     source ~/.bashrc
 fi
+
+# Deactivate the virtual environment
+deactivate
