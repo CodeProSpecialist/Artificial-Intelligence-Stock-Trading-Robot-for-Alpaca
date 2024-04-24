364355a224844d027ffe32dac7c0d4169736d739
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2024-04-23 20:50:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,29 +3,8 @@
 # Update package list
 sudo apt update
 
-# Install Python 3 and pip3
-sudo apt install -y python3 python3-pip
-
 # Install required packages
 sudo apt install -y libhdf5-dev
-
-# Create and activate virtual environment in the user's home directory
-HOME_VENV_PATH="$HOME/My-Python-Virtual-Environment-Packages"
-VENV_PATH="$HOME_VENV_PATH/venv"
-
-# Check if the virtual environment directory exists, if not, create it
-if [ ! -d "$VENV_PATH" ]; then
-    mkdir -p "$VENV_PATH"
-fi
-
-# Create the virtual environment
-python3 -m venv "$VENV_PATH"
-
-# Activate the virtual environment
-source "$VENV_PATH/bin/activate"
-
-# Install Python packages within the virtual environment
-pip install yfinance numpy scikit-learn alpaca-trade-api pytz ta-lib torch torchvision
 
 # Install TA-Lib dependencies
 echo "Installing TA-Lib dependencies ..."
@@ -41,21 +20,14 @@ echo "Configuring TA-Lib..."
 echo "Installing TA-Lib..."
 make
 sudo make install
-
 cd ..
 rm -r ta-lib
 rm ta-lib-0.4.0-src.tar.gz
 
-# Add the virtual environment's bin directory to PATH if not already present
-if ! grep -q "$HOME_VENV_PATH/venv/bin" ~/.bashrc; then
-    echo 'export PATH="$PATH:'"$HOME_VENV_PATH"'/venv/bin"' >> ~/.bashrc
-    source ~/.bashrc
-fi
+# Update Python packages using Anaconda's pip (assuming Anaconda is already installed)
+conda activate
+pip3 install yfinance numpy scikit-learn alpaca-trade-api pytz ta-lib torch torchvision
 
-# Deactivate the virtual environment
-deactivate
-
-# Inform the user about the virtual environment
-echo "Your Python commands in the directory named My-Python-Virtual-Environment-Packages will be the Python commands that run this installed virtual environment's Python programs."
-
-echo "just open the folder named My-Python-Virtual-Environment-Packages/venv/bin and run pip3 to install anything else with the command ./pip3 and then run python3 with the command ./python3  "
+# Inform the user about Anaconda installation
+echo "Your Python commands will be the Python commands that run with Anaconda's Python programs."
+echo "You can activate Anaconda by running 'conda activate' and then install anything else with pip."
